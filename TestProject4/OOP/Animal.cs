1f40ae9313f3e0b504b8bdf16249b5410351214f
Гпригорий Мануковский <grisha.manuk123@yandex.ru>
2024-06-07 15:52:43 +0300

    oop

--- TestProject4/OOP/Animal.cs
+++ TestProject4/OOP/Animal.cs
@@ -6,13 +6,15 @@
         //Инкапсуляция это скрытие каких либо данных/методов
         private string yelling = "Очень громко орет";
         public string _name { get; }
-        public Animal(string name) {
+        public Animal(string name)
+        {
             _name = name;
         }
         public string GetYelling() { return yelling; }
         //Если все же нужен доступ к инкопсулированым данным, стоит делать отдельный метод для изменения
-        public void SetYelling(string value) { if(value == "Матершино орет")yelling = value; }
-        public void YELLING() {
+        public void SetYelling(string value) { if (value == "Матершино орет") yelling = value; }
+        public void YELLING()
+        {
             Console.WriteLine($"{_name} {yelling}");
         }
     }
@@ -22,6 +24,17 @@
         public Cat(string name) : base(name) { }
         //Порода кошки
         public string Breed { get; set; }
+    }
+    public class TestsOOP
+    {
+
+        [Test]
+        public void TestOOP()
+        {
+            //Иницилизируем класс Cat
+            Animal cat = new Cat("Сиамский");
+            cat.YELLING();
+        }
     }
 
 }
